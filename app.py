528252e4cb2f5207ee57bcf629b0b0a1650c2e5e
kamirazio <kamirazio@gmail.com>
2017-12-17 20:00:34 +0900

    DBを作成してみる

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 from flask import Flask, render_template, request
 # データベースモジュールのインポート
 from tinydb import TinyDB, Query
-# データベースの作成
-db = TinyDB('task_db.json')
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -16,6 +15,25 @@ def add():
     # /add にルーティングされた時に、GETで受け取ったリクエストをデータベースに保存していく
     # 保存が終わったら、indexを実行しJSONに出力
     return index()
+
+@app.route("/reset")
+def reset():
+    # ここにアクセスすると、最初のダミーデータが生成される
+
+    # データベースの作成
+    db = TinyDB('task_db.json')
+    if db is not None:
+        db.purge()
+
+    # データの格納
+    db.insert({'task': 'apple'})
+    db.insert({'task': 'peach'})
+    db.insert({'task': 'banana'})
+    db.insert({'task': 'orenge'})
+
+    db.all()
+
+    return "reset"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
